41a232ab6861ba35381653b7660beccf36504ee0
Sam Gammon <sam@elide.ventures>
2023-11-09 15:48:38 -0800

    feat: add driver implementations
    - feat: add logic for `mysqlj` driver impl
    - test: obtain `mysqlj` driver, connection string tests
    - chore: update module pins
    - chore: configure codecov
    
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- subprojects/impl-mysqlj/src/test/kotlin/com/planetscale/jvm/mysqlj/PlanetscaleMysqlDriverTest.kt
+++ subprojects/impl-mysqlj/src/test/kotlin/com/planetscale/jvm/mysqlj/PlanetscaleMysqlDriverTest.kt
@@ -1,9 +1,8 @@
 package com.planetscale.jvm.mysqlj
 
+import com.planetscale.jvm.PlanetscaleConfig
 import org.junit.jupiter.api.assertDoesNotThrow
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
+import kotlin.test.*
 
 /** Tests for the MySQL/J connector for the Planetscale driver. */
 class PlanetscaleMysqlDriverTest {
@@ -53,5 +52,155 @@ class PlanetscaleMysqlDriverTest {
             "something-custom.com",
             PlanetscaleMysqlDriver.resolveHostSymbols("something-custom.com"),
         )
+    }
+
+    @Test fun testCalculateMySQLURI() = PlanetscaleMysqlDriver().let {
+        assertNotNull(it, "should be able to construct mysql driver impl from scratch")
+
+        val config = PlanetscaleConfig.of(
+            PlanetscaleConfig.parseUri("jdbc:planetscale://aws/hello"),
+            com.planetscale.jvm.PlanetscaleCredential.user("hello", "user", "pass"),
+        )
+        assertDoesNotThrow {
+            it.configure(config)
+        }
+        assertDoesNotThrow {
+            it.validate()
+        }
+        val uri = assertDoesNotThrow {
+            it.buildUri(config)
+        }
+        assertNotNull(uri, "should not get `null` DB URI from database driver")
+        val str = uri.toString()
+        assertFalse(str.startsWith("jdbc:"), "calculated DB URL should NOT have `jdbc` prefix")
+        assertTrue(str.startsWith("mysql:"), "calculated DB URL should have `mysql:` prefix")
+        val hosts = str
+            .substringAfter("://")
+            .substringAfter("@")
+            .substringBefore("/")
+            .split(",")
+        assertEquals(1, hosts.size, "should find one host in single-host URL")
+        assertEquals("aws.connect.psdb.cloud:3306", hosts.first(), "host should be expected value")
+        assertEquals("/hello", uri.path, "DB name should be included properly")
+    }
+
+    @Test fun testCalculateMySQLURIWithCredential() = PlanetscaleMysqlDriver().let {
+        assertNotNull(it, "should be able to construct mysql driver impl from scratch")
+
+        val config = PlanetscaleConfig.resolve(
+            PlanetscaleConfig.parseUri("jdbc:planetscale://user:pass@aws/hello"),
+        )
+        assertDoesNotThrow {
+            it.configure(config)
+        }
+        assertDoesNotThrow {
+            it.validate()
+        }
+        val uri = assertDoesNotThrow {
+            it.buildUri(config)
+        }
+        assertNotNull(uri, "should not get `null` DB URI from database driver")
+        val str = uri.toString()
+        assertFalse(str.startsWith("jdbc:"), "calculated DB URL should NOT have `jdbc` prefix")
+        assertTrue(str.startsWith("mysql:"), "calculated DB URL should have `mysql:` prefix")
+        val hosts = str
+            .substringAfter("://")
+            .substringAfter("@")
+            .substringBefore("/")
+            .split(",")
+
+        assertNotNull(uri.userInfo, "should be able to find user info in credential-based URI")
+        assertEquals(
+            "user",
+            uri.userInfo.split(":").first(),
+            "should find expected user in credential-based URI"
+        )
+        assertEquals(
+            "pass",
+            uri.userInfo.split(":").last(),
+            "should find expected password in credential-based URI"
+        )
+        assertEquals(1, hosts.size, "should find one host in single-host URL")
+        assertEquals("aws.connect.psdb.cloud:3306", hosts.first(), "host should be expected value")
+        assertEquals("/hello", uri.path, "DB name should be included properly")
+    }
+
+    @Test fun testRenderConnectionStringWithCredentials() = PlanetscaleMysqlDriver().let {
+        assertNotNull(it, "should be able to construct mysql driver impl from scratch")
+
+        val config = PlanetscaleConfig.resolve(
+            PlanetscaleConfig.parseUri("jdbc:planetscale://user:pass@aws/hello"),
+        )
+        assertDoesNotThrow {
+            it.configure(config)
+        }
+        assertDoesNotThrow {
+            it.validate()
+        }
+        val uri = assertDoesNotThrow {
+            it.renderedConnectionString(config)
+        }
+        assertNotNull(uri, "should not get `null` DB URI from database driver")
+        assertTrue(
+            uri.startsWith("jdbc:"),
+            "rendered DB connection string should have `jdbc` prefix, but got: $uri",
+        )
+        assertTrue(
+            uri.startsWith("jdbc:mysql:"),
+            "rendered DB connection string should have `jdbc:mysql:` prefix, but got: $uri",
+        )
+        val userInfo = uri
+            .substringAfter("://")
+            .substringBefore("@")
+        val user = userInfo.split(":").first()
+        val pass = userInfo.split(":").last()
+        val dbname = uri
+            .substringAfter("://")
+            .substringAfter("@")
+            .substringAfter("/")
+            .substringBefore("?")
+        val hosts = uri
+            .substringAfter("://")
+            .substringAfter("@")
+            .substringBefore("/")
+            .split(",")
+
+        assertNotNull(userInfo, "should be able to find user info in credential-based connection string")
+        assertEquals(
+            "user",
+            user,
+            "should find expected user in credential-based connection string"
+        )
+        assertEquals(
+            "pass",
+            pass,
+            "should find expected password in credential-based connection string"
+        )
+        assertEquals(1, hosts.size, "should find one host in single-host URL")
+        assertEquals("aws.connect.psdb.cloud:3306", hosts.first(), "host should be expected value")
+        assertEquals("hello", dbname, "DB name should be included in connection string")
+    }
+
+    @Test fun testDriverInterface(): Unit = PlanetscaleMysqlDriver().let {
+        assertNotNull(it.getMajorVersion(), "major version should not be null")
+        assertNotNull(it.getMinorVersion(), "major version should not be null")
+    }
+
+    @Test fun testCreateUnderlyingDriver(): Unit = PlanetscaleMysqlDriver().let {
+        val config = PlanetscaleConfig.resolve(
+            PlanetscaleConfig.parseUri("jdbc:planetscale://user:pass@aws/hello"),
+        )
+        assertDoesNotThrow {
+            it.configure(config)
+        }
+        assertDoesNotThrow {
+            it.validate()
+        }
+        assertDoesNotThrow {
+            it.renderedConnectionString(config)
+        }
+        assertDoesNotThrow {
+            assertNotNull(it.createDriver())
+        }
     }
 }
