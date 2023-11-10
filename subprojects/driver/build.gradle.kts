74b6768b6c3bd76b938119789e0da0528354c92c
Sam Gammon <sam@elide.ventures>
2023-11-09 18:15:32 -0800

    chore: finish `driver` package refactor
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- subprojects/driver/build.gradle.kts
+++ subprojects/driver/build.gradle.kts
@@ -3,14 +3,21 @@ import PlanetscaleBuild.publishable
 plugins {
     id("common-conventions.kotlin")
     id("planetscale-publishable.klib")
+    alias(libs.plugins.ksp)
     id(libs.plugins.sonar.get().pluginId)
 }
 
 dependencies {
+    api(libs.bundles.slf4j)
+    api(kotlin("stdlib"))
     api(projects.subprojects.coreApi)
+    api(libs.bundles.mysql)
     implementation(projects.subprojects.implMysqlj)
+
     testImplementation(libs.bundles.junit5)
     testImplementation(testFixtures(projects.subprojects.coreApi))
+    testRuntimeOnly(projects.subprojects.implH2)
+    testApi(libs.bundles.h2)
 }
 
 kotlin {
@@ -21,3 +28,7 @@ publishable(
     name = PlanetscaleBuild.Library.DRIVER,
     description = "Planetscale JDBC meta-driver",
 )
+
+tasks.test.configure {
+    jvmArgs("-Dplanetscale.driver=com.planetscale.jvm.h2.PlanetscaleH2Driver")
+}
