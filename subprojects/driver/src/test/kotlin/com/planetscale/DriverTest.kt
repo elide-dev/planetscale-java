d09d2d28c262b8b487a759e75f0c6b8342e487e1
Sam Gammon <sam@elide.ventures>
2023-11-09 18:23:10 -0800

    test: add test for `acceptsURL`
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- subprojects/driver/src/test/kotlin/com/planetscale/DriverTest.kt
+++ subprojects/driver/src/test/kotlin/com/planetscale/DriverTest.kt
@@ -1,11 +1,10 @@
 package com.planetscale
 
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import java.util.ServiceLoader
-import kotlin.test.Test
-import kotlin.test.assertNotEquals
-import kotlin.test.assertNotNull
-import kotlin.test.assertTrue
+import kotlin.test.*
 
 /** Tests for the main driver facade, at [com.planetscale.Driver]. */
 class DriverTest {
@@ -46,5 +45,30 @@ class DriverTest {
 
         assertNotEquals(0, all.size, "should have at least one driver")
         assertTrue(all.contains(Driver::class), "should contain our driver")
+    }
+
+    @Test fun testDriverCompliance() {
+        assertTrue(testDriver.jdbcCompliant(), "driver should indicate jdbc compliance")
+    }
+
+    @Test fun testDriverUse(): Unit = Driver().use {
+        assertNotNull(it, "should be able to obtain test driver and close it")
+    }
+
+    @CsvSource(
+        "jdbc:planetscale://aws/hello,true",
+        "jdbc:planetscale://user:pass@aws/hello,true",
+        "jdbc:planetscale://user:pass@aws/hello?something=hi,true",
+        "jdbc:mysql://testing123/hello,false",
+        "jdbc:mysql://user:pass@aws/hello,false",
+        "jdbc:mysql://user:pass@aws/hello?something=hi,false",
+    )
+    @ParameterizedTest
+    fun testAcceptsUrl(url: String, accepts: Boolean) {
+        assertEquals(
+            accepts,
+            testDriver.acceptsURL(url),
+            "`acceptsURL` should behave as expected"
+        )
     }
 }
